742f082728033b05a74e0b4b9394f04d83ba5d16
Janki <janek935@gmail.com>
2019-12-05 20:56:12 +0100

    Implementacja pocisków (in progress)

---
+++ Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : Projectile
+{
+    [SerializeField] List<ParticleSystem> onHitParticles = new List<ParticleSystem>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        playOnHitParticles();
+    }
+
+    void playOnHitParticles ()
+    {
+        if (onHitParticles != null)
+        {
+            for (int i = 0; i < onHitParticles.Count; i++)
+            {
+                if (onHitParticles[i] != null)
+                {
+                    onHitParticles[i].Play();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public bool IsActive
+    {
+        get;
+        private set;
+    }
+
+    public virtual void Shoot (Vector3 origin, Vector3 forward, Vector3 up)
+    {
+
+    }
+    public virtual float GetBaseSpeed (){ return 0f; }
+
+    private void Update()
+    {
+        if (IsActive)
+        {
+
+        }
+    }
+}
